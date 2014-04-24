a8eeff23aa462c6a1fdae462ab5037dcf0a83deb
Asim Mushtaq <asim.mushtaq@arkhitech.com>
2014-04-24 14:07:59 +0500

    adds model to the music feed class

--- app/models/musicfeed.rb
+++ app/models/musicfeed.rb
@@ -1,2 +1,15 @@
 class Musicfeed < ActiveRecord::Base
+   
+  validates_presence_of :wrapperType, :kind, :artistId, :collectionId, :trackId,
+    :artistName, :collectionName, :trackName , :collectionCensoredName,
+    :trackCensoredName ,:artistViewUrl, :collectionViewUrl , :trackViewUrl ,
+    :previewUrl, :artworkUrl30 ,:artworkUrl60 , :artworkUrl100 , :collectionPrice,
+    :trackPrice, :releaseDate, :collectionExplicitness , :trackExplicitness,
+    :discCount, :discNumber, :trackCount, :trackNumber, :trackTimeMillis,
+    :country, :currency, :primaryGenreName, :radioStationUrl
+  
+
+
+    validates_uniqueness_of :trackViewUrl
+
 end
